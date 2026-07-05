79226829bf9c7b73b98fa61ecfb7f099c1108baa
Mandvi Singh <mandvisingh28333333@gmail.com>
2026-07-05 23:31:51 +0530

    Day 2: Added JWT authentication and user APIs

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 // Middleware
@@ -11,5 +13,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Server Running...");
 });
+
+// Authentication Routes
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
